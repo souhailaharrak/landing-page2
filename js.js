a44505805afe23e8f9f87daffe0333b8aef194cc
souhailaharrak <92025114+souhailaharrak@users.noreply.github.com>
2022-04-03 21:41:28 +0000

    Add files via upload

---
+++ js.js
@@ -0,0 +1,42 @@
+// var arr = [-1, 2, 5, -7, -5];
+// function ray() {
+
+//     var somme = 0;
+//     for (i = 0; i < arr.length; i++) {
+//         if (arr[i] > 0) {
+//             somme += arr[i]
+
+//         }
+//     } document.write(somme);
+
+
+
+// } ray();
+
+// function add(name){
+     
+//     var myadd=name.split(" ");
+//         return (myadd[0][0].toUpperCase() + " ."+ myadd[1][0].toUpperCase());
+ 
+// }   document.write(add("harak souhaila"));
+
+// function disemvowel(str) {
+  
+//     var st=str.replace(i,'.')
+    
+//     return str;
+    
+
+//   }  
+// function disemvowel(str) {
+
+//     var str = str.replace(/a/gi,'').replace(/e/gi,'').replace(/i/gi,'').replace(/o/gi,'').replace(/u/gi,'');
+  
+//     return str;
+  
+//   } document.write(disemvowel("AKHI")); 
+
+
+
+
+
